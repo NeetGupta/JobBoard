77edb5f4c113c64f96f4aeede2dbcaf23b89e37a
NeetGupta <neetgupta702@gmail.com>
2025-08-19 23:39:21 +0530

    Create AppDbContext.cs

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using JobBoard.Models;
+
+namespace JobBoard.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
